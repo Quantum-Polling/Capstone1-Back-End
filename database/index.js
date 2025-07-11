018807a993add985d6d63394cea70d4530dab221
Joseph <sourpina@gmail.com>
2025-07-11 19:19:17 -0400

    Created Poll model and Poll-User association
    Closes #16

--- database/index.js
+++ database/index.js
@@ -1,7 +1,16 @@
 const db = require("./db");
 const User = require("./user");
+const Poll = require("./poll");
+
+// Create one to many relationship between User and Poll
+Poll.belongsTo(User, { as: "creator"});
+User.hasMany(Poll, { foreignKey: {
+  name: "creatorId",
+  allowNull: false,
+}});
 
 module.exports = {
   db,
   User,
+  Poll,
 };
