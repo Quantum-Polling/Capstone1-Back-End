3bd74116e09d0deee57c809f26194e2104cecfcb
Joseph <sourpina@gmail.com>
2025-07-11 19:22:50 -0400

    Created the Poll Option model and Poll-Poll Option association
    Closes #17

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const db = require("./db");
 const User = require("./user");
 const Poll = require("./poll");
+const PollOption = require("./poll_option");
 
 // Create one to many relationship between User and Poll
 Poll.belongsTo(User, { as: "creator"});
@@ -9,8 +10,16 @@ User.hasMany(Poll, { foreignKey: {
   allowNull: false,
 }});
 
+// Create one to many relationship between Poll and PollOption
+PollOption.belongsTo(Poll);
+Poll.hasMany(PollOption, {foreignKey: {
+  name: "pollId",
+  allowNull: false,
+}});
+
 module.exports = {
   db,
   User,
   Poll,
+  PollOption,
 };
