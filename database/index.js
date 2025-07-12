d1751835dbfb218c005befbabceecdb139dffb44
Joseph <sourpina@gmail.com>
2025-07-11 22:14:09 -0400

    Created Poll Vote model, constraints, and associations

--- database/index.js
+++ database/index.js
@@ -2,6 +2,7 @@ const db = require("./db");
 const User = require("./user");
 const Poll = require("./poll");
 const PollOption = require("./poll_option");
+const PollVote = require("./poll_vote");
 
 // Create one to many relationship between User and Poll
 Poll.belongsTo(User, { as: "creator"});
@@ -17,9 +18,30 @@ Poll.hasMany(PollOption, {foreignKey: {
   allowNull: false,
 }});
 
+// Create (zero or one) to many relationship between User and PollVote
+PollVote.belongsTo(User, { as: "voter", foreignKey: {
+  name: "userId",
+  allowNull: true,
+}});
+User.hasMany(PollVote);
+
+// Create one to many relationship between PollOption and PollVote
+PollVote.belongsTo(PollOption, { foreignKey: {
+  name: "optionId",
+  allowNull: false,
+}});
+PollOption.hasMany(PollVote);
+
+// Create a one to many relationship between Poll and PollVote
+PollVote.belongsTo(Poll, { foreignKey: {
+  name: "pollId",
+  allowNull: false,
+}});
+
 module.exports = {
   db,
   User,
   Poll,
   PollOption,
+  PollVote,
 };
---
+++ database/poll_vote.js
@@ -0,0 +1,42 @@
+const { DataTypes, ValidationError } = require("sequelize");
+const db = require("./db");
+const PollOption = require("./poll_option");
+
+const PollVote = db.define("poll_vote", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  rank: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  submitted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  },
+}, 
+{
+  indexes: [
+    {
+      unique: true,
+      fields: ['userId', 'optionId'],
+    },
+    {
+      unique: true,
+      fields: ['userId', 'pollId', 'rank'],
+    }
+  ],
+  validate: {
+    async matchOptionToPoll() {
+      const option = PollOption.findByPk(this.optionId);
+      if (this.pollId !== option.pollId)
+        throw new ValidationError("Option does not belong to the specified poll");
+    }
+  }
+}
+);
+
+module.exports = PollVote;
