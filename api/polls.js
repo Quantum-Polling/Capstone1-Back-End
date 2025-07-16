9641b6ddd33aca921d7374a34169e5fdfc07166b
Joseph Collado <sourpina@gmail.com>
2025-07-16 15:03:59 -0400

    Added poll patch route with basic validation

--- api/polls.js
+++ api/polls.js
@@ -44,4 +44,39 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Edit a poll
+router.patch("/:userId/:id", async (req, res) => {
+  try {
+    const pollInfo = req.body;
+    const pollId = Number(req.params.id);
+    const userId = Number(req.params.userId);
+    const poll = await Poll.findByPk(pollId);
+
+    // Validate that the poll to be edited exists
+    if (!poll)
+      return res.status(404).send({ error: `Poll with ID ${pollId} not found` });
+
+    // Validate that the user is allowed to edit this poll
+    if (poll.creatorId !== userId)
+      return res.status(403).send({ error: "You are not the creator of this poll and thus cannot edit it" });
+
+    // Validate that the poll can be edited
+    if (poll.status !== "Draft")
+      return res.status(403).send({ error: `Poll with ID ${pollId} was already published and cannot be edited` });
+
+    // Update the poll
+    poll.update({
+      title: pollInfo.title,
+      description: pollInfo.description,
+      status: pollInfo.status,
+      closeDate: pollInfo.closeDate,
+      authVotes: !pollInfo.open,
+    });
+
+    res.status(200).send({ message: "Succesfully updated poll" });
+  } catch (error) {
+    res.status(500).send({ error: `Error updating poll: ${error}` });
+  }
+});
+
 module.exports = router;
