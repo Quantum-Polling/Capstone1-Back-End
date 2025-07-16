7598b362b22233152291c88c991bba85b589d7d5
Joseph Collado <sourpina@gmail.com>
2025-07-16 14:42:20 -0400

    Adjusted create poll route to account for drafts

--- api/polls.js
+++ api/polls.js
@@ -11,21 +11,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Publish a new poll
+// Create a new poll
 router.post("/", async (req, res) => {
   try {
     const pollInfo = req.body;
 
-    const newPoll = await Poll.create({
+    // Validate that this user doesn't already have a poll with this title
+    const duplicate = await Poll.findOne({
+      where: { 
+        creatorId: pollInfo.creatorId,
+        title: pollInfo.title 
+      }
+    });
+    if (duplicate)
+      return res.status(409).send("A poll with this title already exists for this user");
+
+    const poll = await Poll.create({
       title: pollInfo.title,
       description: pollInfo.description,
-      status: "Open",
-      closeDate: pollInfo.closeDate ? pollInfo.closeDate : null,
+      status: pollInfo.status,
+      closeDate: pollInfo.closeDate,
       authVotes: !pollInfo.open,
       creatorId: pollInfo.creatorId,
     });
 
-    res.status(201).send({ pollId: newPoll.id });
+    if (pollInfo.status === "Draft")
+      res.status(201).send({ message: "Successfully saved new poll draft" });
+    else
+      res.status(201).send({ message: "Successfully published new poll", pollId: poll.id });
   } catch (error) {
     res.status(500).send("Error creating new poll:", error);
   }
