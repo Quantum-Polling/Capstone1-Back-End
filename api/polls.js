879293fa9527ccca883ccebfb6d370ade67bc9f0
Joseph Collado <sourpina@gmail.com>
2025-07-16 14:29:30 -0400

    Added basic new poll creation

--- api/polls.js
+++ api/polls.js
@@ -11,4 +11,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Publish a new poll
+router.post("/", async (req, res) => {
+  try {
+    const pollInfo = req.body;
+
+    const newPoll = await Poll.create({
+      title: pollInfo.title,
+      description: pollInfo.description,
+      status: "Open",
+      closeDate: pollInfo.closeDate ? pollInfo.closeDate : null,
+      authVotes: !pollInfo.open,
+      creatorId: pollInfo.creatorId,
+    });
+
+    res.status(201).send({ pollId: newPoll.id });
+  } catch (error) {
+    res.status(500).send("Error creating new poll:", error);
+  }
+});
+
 module.exports = router;
