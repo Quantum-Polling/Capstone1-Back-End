d8c742720dcd5a89ce546de47c48ad6926363a2a
Joseph Collado <sourpina@gmail.com>
2025-07-17 11:44:04 -0400

    Added publish verification method

--- api/polls.js
+++ api/polls.js
@@ -2,6 +2,51 @@ const express = require("express");
 const router = express.Router();
 const { Poll, PollOption } = require("../database");
 
+// Validates if the given poll can be published
+// Returns an object with the boolean 'publishable' and
+// an array 'errors' with any errors if not publishable
+const isPublishable = async (poll) => {
+  const response = {
+    publishable: false,
+    errors: []
+  };
+  
+  // Only verify polls that are meant to be published
+  if (poll.status !== "Open")
+    response.errors = [...response.errors, "Poll is not marked to be published"];
+
+  // Verify that there are at least two options
+  if (!poll.options || poll.options.length < 2)
+    response.errors = [...response.errors, "Not enough options to publish this poll"];
+
+  // Verify that the user doesn't already have a poll with the given title
+  const duplicate = await Poll.findOne({
+      where: { 
+        creatorId: poll.creatorId,
+        title: poll.title 
+      }
+    });
+  if (duplicate)
+    response.errors = [...response.errors, "A poll with this title already exists for this user"];
+
+  // Verify that the title, description, and options are all not empty
+  if (!title.trim())
+    response.errors = [...response.errors, "Title cannot be empty"];
+
+  if (!description.trim())
+    response.errors = [...response.errors, "Description cannot be empty"];
+
+  for (let i = 0; i < poll.options.length; i++)
+    if (!option[i].trim())
+      response.errors = [...response.errors, `Option ${i + 1} cannot be empty`];
+  
+  // All verifications passed
+  if (response.errors.length === 0)
+    response.publishable = true;
+
+  return response;
+}
+
 router.get("/", async (req, res) => {
   try {
     const result = await Poll.findAll();
@@ -15,10 +60,21 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const pollInfo = req.body;
+    // Published
+    // Verify that there are at least two options
+    // Verify that the user doesn't already have a poll with the given title
+    // Verify that the title, description, and each option are not empty
+    // Create new poll
+    // Create new options
+
+    // Draft
+    // Fill any empty fields (title, description, or options) with placeholders
+    // Create new poll
+    // Create new options
 
     // Validate that there are at least 2 options if the user is publishing
     if (pollInfo.options && pollInfo.options.length < 2 && pollInfo.status === "Open")
-      return res.status(422).send({ message: "Not enough options to publish poll" });
+      return res.status(422).send({ error: "Not enough options to publish poll" });
 
     // Validate that this user doesn't already have a poll with this title
     const duplicate = await Poll.findOne({
@@ -28,7 +84,7 @@ router.post("/", async (req, res) => {
       }
     });
     if (duplicate)
-      return res.status(409).send({ message: "A poll with this title already exists for this user" });
+      return res.status(409).send({ error: "A poll with this title already exists for this user" });
 
     const poll = await Poll.create({
       title: pollInfo.title,
@@ -64,6 +120,25 @@ router.patch("/:userId/:id", async (req, res) => {
     const userId = Number(req.params.userId);
     const poll = await Poll.findByPk(pollId);
     
+    // Verify Poll exists
+    // Verify Poll is in draft state
+    // Verify User is the creator of the Poll
+
+    // Published
+    // Verify that there are at least two options
+    // Verify that the user doesn't already have a poll with the given title
+    // Verify that the title, description, and each option are not empty
+    // Update existing poll data
+    // Update existing options text
+    // - If more options exist in the database than were published, delete the extra ones
+    // - If more options are published than exist in the database, add the new ones
+
+    // Draft
+    // Fill any empty fields (title, description, or options) with placeholders
+    // Update existing poll data
+    // Update existing options text
+    // - If more options are published than exist in the database, add the new ones
+
     // Validate that this user doesn't already have a poll with this title
     const duplicate = await Poll.findOne({
       where: { 
